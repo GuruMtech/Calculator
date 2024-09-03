830a5341825c608e4b425067aea300002ec69724
GuruMtech <gurumoorthy2500@gmail.com>
2024-09-03 23:08:49 +0530

    Initial commit

---
+++ src/Components/AnimatedText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AnimatedText.css';
+
+const AnimatedText = () => {
+  return (
+    <div className="calculator-header">
+      <h1>Calculator</h1>
+    </div>
+  );
+};
+
+export default AnimatedText;
